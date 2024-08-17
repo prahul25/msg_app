cd06479d8a705a46965fa579cdf68b3ceb8e1b21
Rahul Pali <141947406+prahul25@users.noreply.github.com>
2024-08-17 13:14:49 +0530

    Update page.tsx

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
       </Carousel>
     </main>
           <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-          © 2023 True Feedback. All rights reserved.
+          © 2024 True Feedback. All rights reserved.
         </footer>
         </>
   
