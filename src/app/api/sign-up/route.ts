d6c35ddccf494e70627fd45fb56cf9e34c302a3e
prahul25 <palrahulkumar2002@gmail.com>
2024-08-22 20:10:02 +0530

    Fix major while providing feedback/message

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,7 +1,8 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User.model";
 import bcrypt from "bcryptjs";
-import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
 
 
 export async function POST(request: Request) {
